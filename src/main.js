9de90f378482fa91f3c746ad65b553e649ddf3e8
Aslam <92069554+Wasabi-Popcorn@users.noreply.github.com>
2021-11-04 19:17:52 +0530

    Test Update
    .env command to fit both environment (replit, vscode-local)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const botInfo = require("./info.json");
 require('discord-reply');
+require('dotenv').config();
 
 console.log("From APP : Bot starting up");
 const client = new Discord.Client();
@@ -12,32 +13,32 @@ client.commands = new Discord.Collection();
 const commandFiles = botInfo.commandFiles
 
 for (const f of commandFiles) {
-  const command = require(`./commands/${f}`);
-  client.commands.set(command.name, command);
+    const command = require(`./commands/${f}`);
+    client.commands.set(command.name, command);
 }
 
 // Connection setup
 client.once("ready", () => {
-  console.log("Bot Online !!!");
+    console.log("Bot Online !!!");
 });
 
 client.on("message", (msg) => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-  const args = msg.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    client.commands.get("ping").execute(msg, args);
-  } else if (command === "google" || command === "g") {
-    client.commands.get("google").execute(msg, args);
-  } else if (command === "delete" || command === "del") {
-    client.commands.get("delete").execute(msg, args, client);
-  } else if (command === "help" || command === "h") {
-    client.commands.get("help").execute(msg, args, commandFiles);
-  } else {
-    msg.channel.send("bonk !");
-  }
+    if (command === "ping") {
+        client.commands.get("ping").execute(msg, args);
+    } else if (command === "google" || command === "g") {
+        client.commands.get("google").execute(msg, args);
+    } else if (command === "delete" || command === "del") {
+        client.commands.get("delete").execute(msg, args, client);
+    } else if (command === "help" || command === "h") {
+        client.commands.get("help").execute(msg, args, commandFiles);
+    } else {
+        msg.channel.send("bonk !");
+    }
 });
 
-client.login(process.env['BOT_TOKEN']);
+client.login(process.env.BOT_TOKEN);
